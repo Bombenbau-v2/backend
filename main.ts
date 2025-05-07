f87945644fafb7b5ed472d50100994453eee709f
Liam Gruber <contact@dnascaner.de>
2025-05-07 14:40:57 +0200

    Added ability to change display name and tag

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 import {checkHash} from "./methods/hash.ts";
 import type {RegisterRequest, RegisterResponse} from "./types/http.ts";
 import type {Session, User} from "./types/misc.ts";
-import type {LoginRequest, LoginResponse, SocketRequest} from "./types/ws.ts";
+import type {ChangeDisplayNameRequest, ChangeDisplayNameResponse, ChangeTagRequest, ChangeTagResponse, LoginRequest, LoginResponse, SocketRequest} from "./types/ws.ts";
 
 type JSONParseError = "JSON_PARSE_ERROR";
 
@@ -16,7 +16,6 @@ const safeJSONParse = (text: string, logError?: boolean): JSONParseError | any =
 };
 
 const users: User[] = [];
-
 const sessions: Session[] = [];
 
 const loadData = () => {
@@ -60,40 +59,115 @@ Deno.serve({port: 6969}, async (req: Request) => {
 
 				console.log("I ->", request);
 
-				// Check if the request follows the base structure
+				// Ensure request structure
 				if (!request.request || !request.data) {
 					return socket.send("malformed_request");
 				}
 
-				if (request.request === "/login") {
-					const data: LoginRequest = request.data;
+				// Reject if unauthorized and not trying to login
+				if (!session.user && request.request !== "/login") return socket.send("unauthorized");
 
-					// Check if the request has all the required fields
+				if (request.request === "/login") {
+					const data = request.data as unknown as LoginRequest;
+
+					// Ensure required fields
 					if (!data.tag || !data.password) {
-						return socket.send(JSON.stringify({success: false, error: "missing_fields"} as LoginResponse));
+						const response: LoginResponse = {concern: "login", success: false, error: "missing_fields"};
+
+						return socket.send(JSON.stringify(response));
 					}
 
 					// Check if there is a user with the provided tag
 					const user = users.find((user) => user.tag === data.tag);
 					if (!user) {
-						return socket.send(JSON.stringify({success: false, error: "user_not_found"} as LoginResponse));
+						const response: LoginResponse = {concern: "login", success: false, error: "user_not_found"};
+
+						return socket.send(JSON.stringify(response));
 					}
 
 					// Check if the password is actually a hash
 					if (!checkHash(data.password)) {
-						return socket.send(JSON.stringify({success: false, error: "malformed_hash"} as LoginResponse));
+						const response: LoginResponse = {concern: "login", success: false, error: "malformed_hash"};
+
+						return socket.send(JSON.stringify(response));
 					}
 
 					// Check if the password is correct
 					if (user.password !== data.password) {
-						return socket.send(JSON.stringify({success: false, error: "wrong_password"} as LoginResponse));
+						const response: LoginResponse = {concern: "login", success: false, error: "wrong_password"};
+
+						return socket.send(JSON.stringify(response));
 					}
 
-					// Save the login info to the session object
+					// Create user reference in session object
 					session.user = user;
 					sessions.push(session);
 
-					socket.send(JSON.stringify({success: true} as LoginResponse));
+					// Send response
+					const response: LoginResponse = {concern: "login", success: true};
+
+					socket.send(JSON.stringify(response));
+				} else if (request.request === "/change_display_name") {
+					const data = request.data as unknown as ChangeDisplayNameRequest;
+
+					// Ensure required fields
+					if (!data.name) {
+						const response: ChangeDisplayNameResponse = {concern: "change_display_name", success: false, error: "missing_fields"};
+
+						return socket.send(JSON.stringify(response));
+					}
+
+					// Check if the name contains any special characters (just a-z0-9-_)
+					if (!/^[a-zA-Z0-9-_ ]+$/.test(data.name)) {
+						const response: ChangeDisplayNameResponse = {concern: "change_display_name", success: false, error: "no_special_characters"};
+
+						return socket.send(JSON.stringify(response));
+					}
+
+					// Ensure name length is between 3 and 16 characters
+					if (data.name.length < 3 || data.name.length > 16) {
+						const response: ChangeDisplayNameResponse = {concern: "change_display_name", success: false, error: "invalid_name_length"};
+
+						return socket.send(JSON.stringify(response));
+					}
+
+					// Update the user's name in the session object
+					session.user!.name = data.name;
+
+					// Send response
+					const response: ChangeDisplayNameResponse = {concern: "change_display_name", success: true};
+
+					socket.send(JSON.stringify(response));
+				} else if (request.request === "/change_tag") {
+					const data = request.data as unknown as ChangeTagRequest;
+
+					// Ensure required fields
+					if (!data.tag) {
+						const response: ChangeTagResponse = {concern: "change_tag", success: false, error: "missing_fields"};
+
+						return socket.send(JSON.stringify(response));
+					}
+
+					// Check if the tag doesn't contain any special characters (just a-z0-9-_)
+					if (!/^[a-zA-Z0-9-_]+$/.test(data.tag)) {
+						const response: ChangeTagResponse = {concern: "change_tag", success: false, error: "no_special_characters"};
+
+						return socket.send(JSON.stringify(response));
+					}
+
+					// Ensure tag length is between 3 and 16 characters
+					if (data.tag.length < 3 || data.tag.length > 16) {
+						const response: ChangeTagResponse = {concern: "change_tag", success: false, error: "invalid_tag_length"};
+
+						return socket.send(JSON.stringify(response));
+					}
+
+					// Check if there is a user with the same tag
+					if (users.some((user) => user.tag === data.tag)) {
+						const response: ChangeTagResponse = {concern: "change_tag", success: false, error: "tag_used"};
+
+						return socket.send(JSON.stringify(response));
+					}
 				}
 			});
 		});
@@ -103,45 +177,62 @@ Deno.serve({port: 6969}, async (req: Request) => {
 
 	const path = new URL(req.url).pathname;
 
+	const headers = new Headers({
+		"Access-Control-Allow-Origin": "*",
+		"Access-Control-Allow-Methods": "*",
+	});
+
 	if (path === "/register") {
 		if (req.method !== "POST") return new Response("Method not allowed", {status: 405});
-		const requestData: RegisterRequest = await safeJSONParse(await req.text());
+		const data: RegisterRequest = await safeJSONParse(await req.text());
 
-		if ((requestData as unknown as JSONParseError) === "JSON_PARSE_ERROR") return new Response("Invalid JSON", {status: 400});
+		if ((data as unknown as JSONParseError) === "JSON_PARSE_ERROR") return new Response("Invalid JSON", {status: 400, headers});
 
 		// Check if the request has all the required fields
-		if (!requestData.name || !requestData.tag || !requestData.password) {
-			return new Response(JSON.stringify({success: false, error: "missing_fields"} as RegisterResponse), {status: 400});
+		if (!data.name || !data.tag || !data.password) {
+			const response: RegisterResponse = {success: false, error: "missing_fields"};
+			return new Response(JSON.stringify(response), {status: 400, headers});
 		}
 
 		// Check if there is a user with the same tag
-		if (users.some((user) => user.tag === requestData.tag)) {
-			return new Response(JSON.stringify({success: false, error: "tag_used"} as RegisterResponse), {status: 409});
+		if (users.some((user) => user.tag === data.tag)) {
+			const response: RegisterResponse = {success: false, error: "tag_used"};
+
+			return new Response(JSON.stringify(response), {status: 409, headers});
 		}
 
 		// Check if the tag doesn't contain any special characters (just a-z0-9-_)
-		if (!/^[a-zA-Z0-9-_]+$/.test(requestData.tag)) {
-			return new Response(JSON.stringify({success: false, error: "no_special_characters"} as RegisterResponse), {status: 400});
+		if (!/^[a-zA-Z0-9-_]+$/.test(data.tag)) {
+			const response: RegisterResponse = {success: false, error: "no_special_characters"};
+
+			return new Response(JSON.stringify(response), {status: 400, headers});
 		}
 
-		// Remove all "dangerous" characters from the name (e.g. <>, /, \, etc.)
-		const sanitizedName = requestData.name.replace(/[^a-zA-Z0-9-_ ]/g, "");
+		// Check if the name doesn't contain any special characters (just a-z0-9-_)
+		if (!/^[a-zA-Z0-9-_:]+$/.test(data.name)) {
+			const response: RegisterResponse = {success: false, error: "no_special_characters"};
+
+			return new Response(JSON.stringify(response), {status: 400, headers});
+		}
 
 		// The password inside of the request is already hashed. Check if the password is actually a hash
-		if (!checkHash(requestData.password)) {
-			return new Response(JSON.stringify({success: false, error: "invalid_hash"} as RegisterResponse), {status: 400});
+		if (!checkHash(data.password)) {
+			const response: RegisterResponse = {success: false, error: "invalid_hash"};
+
+			return new Response(JSON.stringify(response), {status: 400, headers});
 		}
 
 		users.push({
-			name: sanitizedName,
-			tag: requestData.tag,
-			password: requestData.password,
+			name: data.name,
+			tag: data.tag,
+			password: data.password,
 		});
 
 		return new Response(JSON.stringify({success: true} as RegisterResponse));
 	} else
 		return new Response("404 Not Found", {
 			status: 404,
+			headers,
 		});
 });
 
--- types/ws.ts
+++ types/ws.ts
@@ -1,14 +1,38 @@
 export type SocketRequest = {
-	request: "/login";
-	data: LoginRequest;
+	request: "/login" | "/change_display_name" | "/change_tag";
+	data: LoginRequest | ChangeDisplayNameRequest | ChangeTagRequest;
 };
 
+// Login
 export type LoginRequest = {
 	tag: string;
 	password: string;
 };
 
 export type LoginResponse = {
+	concern: "login";
 	success: boolean;
 	error?: "user_not_found" | "malformed_hash" | "wrong_password" | "missing_fields";
+};
+
+// Change display name
+export type ChangeDisplayNameRequest = {
+	name: string;
+};
+
+export type ChangeDisplayNameResponse = {
+	concern: "change_display_name";
+	success: boolean;
+	error?: "missing_fields" | "invalid_name_length" | "no_special_characters";
+};
+
+// Change tag
+export type ChangeTagRequest = {
+	tag: string;
+};
+
+export type ChangeTagResponse = {
+	concern: "change_tag";
+	success: boolean;
+	error?: "tag_used" | "no_special_characters" | "invalid_hash" | "missing_fields" | "invalid_tag_length";
 };
